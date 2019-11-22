50856798ccdd329a5ba1bf32af6da300ceddfea8
liushooter <shooter.hero@gmail.com>
2019-11-22 14:30:35 +0800

    ckb coin

--- src/crypto-tracker.py
+++ src/crypto-tracker.py
@@ -6,10 +6,10 @@ from workflow import Workflow, ICON_WEB, ICON_ERROR, web
 
 def format_strings_from_quote(ticker, quote_data):
     data = quote_data['RAW'][ticker.upper()]['USD']
-    price = '{:,.2f}'.format(data['PRICE'])
-    high = '{:,.2f}'.format(data['HIGH24HOUR'])
-    low = '{:,.2f}'.format(data['LOW24HOUR'])
-    change = '{:,.2f}'.format(data['CHANGEPCT24HOUR'])
+    price = '{:,.4f}'.format(data['PRICE'])
+    high = '{:,.4f}'.format(data['HIGH24HOUR'])
+    low = '{:,.4f}'.format(data['LOW24HOUR'])
+    change = '{:,.4f}'.format(data['CHANGEPCT24HOUR'])
     formatted = {}
     formatted['title'] = '{}: ${} ({}%)'.format(ticker.upper(), price, change)
     formatted['subtitle'] = '24hr high: ${} | 24hr low: ${}'.format(high, low)
@@ -25,7 +25,7 @@ def main(wf):
 
     if query:
         url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + \
-            query.upper() + '&tsyms=USD'
+            query.strip().upper() + '&tsyms=USD'
         r = web.get(url)
         # throw an error if request failed
         # Workflow will catch this and show it to the user
@@ -43,7 +43,7 @@ def main(wf):
                         subtitle='Please try again.',
                         icon=ICON_ERROR)
     else:
-        url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,LTC,BCH&tsyms=USD'
+        url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,CKB&tsyms=USD'
         r = web.get(url)
         # throw an error if request failed
         # Workflow will catch this and show it to the user
@@ -52,30 +52,23 @@ def main(wf):
         formatted = format_strings_from_quote('BTC', result)
         wf.add_item(title=formatted['title'],
                     subtitle=formatted['subtitle'],
-                    arg='https://www.cryptocompare.com/',
+                    arg='https://www.cryptocompare.com/coins/btc/overview',
                     valid=True,
                     icon='icon/btc.png')
 
         formatted = format_strings_from_quote('ETH', result)
         wf.add_item(title=formatted['title'],
                     subtitle=formatted['subtitle'],
-                    arg='https://www.cryptocompare.com/',
+                    arg='https://www.cryptocompare.com/coins/eth/overview',
                     valid=True,
                     icon='icon/eth.png')
 
-        formatted = format_strings_from_quote('LTC', result)
+        formatted = format_strings_from_quote('CKB', result)
         wf.add_item(title=formatted['title'],
                     subtitle=formatted['subtitle'],
-                    arg='https://www.cryptocompare.com/',
+                    arg='https://www.cryptocompare.com/coins/ckb/overview',
                     valid=True,
-                    icon='icon/ltc.png')
-
-        formatted = format_strings_from_quote('BCH', result)
-        wf.add_item(title=formatted['title'],
-                    subtitle=formatted['subtitle'],
-                    arg='https://www.cryptocompare.com/',
-                    valid=True,
-                    icon='icon/bch.png')
+                    icon='icon/ckb.png')
 
     # Send the results to Alfred as XML
     wf.send_feedback()
